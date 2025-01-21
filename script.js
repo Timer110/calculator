a5a40c24232512336dff0844ac8df288d39bf9fa
Timer110 <121953691+Timer110@users.noreply.github.com>
2025-01-21 21:55:39 +0100

    Added Basic HTML Setup and  Operation-Functions in JS

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = 0;
+
+function add (a, b){
+    return a + b;
+};
+
+function subtract (a, b){
+    return a - b;
+};
+
+function multiply (a, b){
+    return a * b;
+};
+
+function divide (a, b){
+    return a / b;
+};
+
+
+
